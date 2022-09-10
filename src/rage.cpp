a896a31e1d4232a3d6ad5ae8c237de5b7ffadd49
alexguirre <alexguirre.aa@gmail.com>
2022-09-10 20:15:40 +0200

    Refactor project

---
+++ src/rage.cpp
@@ -0,0 +1,242 @@
+#include "rage.h"
+
+parManager** parManager::sm_Instance = nullptr;
+
+std::string SubtypeToStr(parMemberType type, uint8_t subtype)
+{
+	switch (type)
+	{
+	case parMemberType::ARRAY:
+		switch (static_cast<parMemberArraySubtype>(subtype))
+		{
+		case parMemberArraySubtype::ATARRAY: return "ATARRAY";
+		case parMemberArraySubtype::ATFIXEDARRAY: return "ATFIXEDARRAY";
+		case parMemberArraySubtype::ATRANGEARRAY: return "ATRANGEARRAY";
+		case parMemberArraySubtype::POINTER: return "POINTER";
+		case parMemberArraySubtype::MEMBER: return "MEMBER";
+		case parMemberArraySubtype::_0x2087BB00: return "_0x2087BB00";
+		case parMemberArraySubtype::POINTER_WITH_COUNT: return "POINTER_WITH_COUNT";
+		case parMemberArraySubtype::POINTER_WITH_COUNT_8BIT_IDX: return "POINTER_WITH_COUNT_8BIT_IDX";
+		case parMemberArraySubtype::POINTER_WITH_COUNT_16BIT_IDX: return "POINTER_WITH_COUNT_16BIT_IDX";
+		case parMemberArraySubtype::VIRTUAL: return "VIRTUAL";
+		}
+		break;
+	case parMemberType::ENUM:
+		switch (static_cast<parMemberEnumSubtype>(subtype))
+		{
+#if RDR3
+		case parMemberEnumSubtype::_64BIT: return "64BIT";
+#endif
+		case parMemberEnumSubtype::_32BIT: return "32BIT";
+		case parMemberEnumSubtype::_16BIT: return "16BIT";
+		case parMemberEnumSubtype::_8BIT: return "8BIT";
+		}
+		break;
+	case parMemberType::BITSET:
+		switch (static_cast<parMemberBitsetSubtype>(subtype))
+		{
+#if RDR3
+		case parMemberBitsetSubtype::_64BIT: return "64BIT";
+#endif
+		case parMemberBitsetSubtype::_32BIT: return "32BIT";
+		case parMemberBitsetSubtype::_16BIT: return "16BIT";
+		case parMemberBitsetSubtype::_8BIT: return "8BIT";
+		case parMemberBitsetSubtype::ATBITSET: return "ATBITSET";
+		}
+		break;
+	case parMemberType::MAP:
+		switch (static_cast<parMemberMapSubtype>(subtype))
+		{
+		case parMemberMapSubtype::ATMAP: return "ATMAP";
+		case parMemberMapSubtype::ATBINARYMAP: return "ATBINARYMAP";
+		}
+		break;
+	case parMemberType::STRING:
+		switch (static_cast<parMemberStringSubtype>(subtype))
+		{
+		case parMemberStringSubtype::MEMBER: return "MEMBER";
+		case parMemberStringSubtype::POINTER: return "POINTER";
+		case parMemberStringSubtype::CONST_STRING: return "CONST_STRING";
+		case parMemberStringSubtype::ATSTRING: return "ATSTRING";
+		case parMemberStringSubtype::WIDE_MEMBER: return "WIDE_MEMBER";
+		case parMemberStringSubtype::WIDE_POINTER: return "WIDE_POINTER";
+		case parMemberStringSubtype::ATWIDESTRING: return "ATWIDESTRING";
+		case parMemberStringSubtype::ATNONFINALHASHSTRING: return "ATNONFINALHASHSTRING";
+		case parMemberStringSubtype::ATFINALHASHSTRING: return "ATFINALHASHSTRING";
+		case parMemberStringSubtype::ATHASHVALUE: return "ATHASHVALUE";
+		case parMemberStringSubtype::ATPARTIALHASHVALUE: return "ATPARTIALHASHVALUE";
+		case parMemberStringSubtype::ATNSHASHSTRING: return "ATNSHASHSTRING";
+		case parMemberStringSubtype::ATNSHASHVALUE: return "ATNSHASHVALUE";
+#if RDR3
+		case parMemberStringSubtype::ATHASHVALUE16U: return "ATHASHVALUE16U";
+#endif
+		}
+		break;
+	case parMemberType::STRUCT:
+		switch (static_cast<parMemberStructSubtype>(subtype))
+		{
+		case parMemberStructSubtype::STRUCTURE: return "STRUCTURE";
+		case parMemberStructSubtype::EXTERNAL_NAMED: return "EXTERNAL_NAMED";
+		case parMemberStructSubtype::EXTERNAL_NAMED_USERNULL: return "EXTERNAL_NAMED_USERNULL";
+		case parMemberStructSubtype::POINTER: return "POINTER";
+		case parMemberStructSubtype::SIMPLE_POINTER: return "SIMPLE_POINTER";
+		}
+		break;
+#if RDR3
+	case parMemberType::GUID:
+		switch (static_cast<parMemberGuidSubtype>(subtype))
+		{
+		case parMemberGuidSubtype::_0xDF7EBE85: return "_0xDF7EBE85";
+		}
+		break;
+#endif
+	default:
+		switch (static_cast<parMemberCommonSubtype>(subtype))
+		{
+		case parMemberCommonSubtype::COLOR: return "COLOR";
+#if RDR3
+		case parMemberCommonSubtype::ANGLE: return "ANGLE";
+#endif
+		}
+	}
+
+	return std::to_string(subtype);
+}
+
+const char* TypeToStr(parMemberType type)
+{
+	switch (type)
+	{
+	case parMemberType::BOOL: return "BOOL";
+	case parMemberType::CHAR: return "CHAR";
+	case parMemberType::UCHAR: return "UCHAR";
+	case parMemberType::SHORT: return "SHORT";
+	case parMemberType::USHORT: return "USHORT";
+	case parMemberType::INT: return "INT";
+	case parMemberType::UINT: return "UINT";
+	case parMemberType::FLOAT: return "FLOAT";
+	case parMemberType::VECTOR2: return "VECTOR2";
+	case parMemberType::VECTOR3: return "VECTOR3";
+	case parMemberType::VECTOR4: return "VECTOR4";
+	case parMemberType::STRING: return "STRING";
+	case parMemberType::STRUCT: return "STRUCT";
+	case parMemberType::ARRAY: return "ARRAY";
+	case parMemberType::ENUM: return "ENUM";
+	case parMemberType::BITSET: return "BITSET";
+	case parMemberType::MAP: return "MAP";
+	case parMemberType::MATRIX34: return "MATRIX34";
+	case parMemberType::MATRIX44: return "MATRIX44";
+	case parMemberType::VEC2V: return "VEC2V";
+	case parMemberType::VEC3V: return "VEC3V";
+	case parMemberType::VEC4V: return "VEC4V";
+	case parMemberType::MAT33V: return "MAT33V";
+	case parMemberType::MAT34V: return "MAT34V";
+	case parMemberType::MAT44V: return "MAT44V";
+	case parMemberType::SCALARV: return "SCALARV";
+	case parMemberType::BOOLV: return "BOOLV";
+	case parMemberType::VECBOOLV: return "VECBOOLV";
+	case parMemberType::PTRDIFFT: return "PTRDIFFT";
+	case parMemberType::SIZET: return "SIZET";
+	case parMemberType::FLOAT16: return "FLOAT16";
+	case parMemberType::INT64: return "INT64";
+	case parMemberType::UINT64: return "UINT64";
+	case parMemberType::DOUBLE: return "DOUBLE";
+#if RDR3
+	case parMemberType::GUID: return "GUID";
+	case parMemberType::_0xFE5A582C: return "_0xFE5A582C";
+	case parMemberType::QUATV: return "QUATV";
+#endif
+	default: return "UNKNOWN";
+	}
+}
+
+const char* TypeToCasedStr(parMemberType type)
+{
+	switch (type)
+	{
+	case parMemberType::BOOL: return "bool";
+	case parMemberType::CHAR: return "char";
+	case parMemberType::UCHAR: return "uchar";
+	case parMemberType::SHORT: return "short";
+	case parMemberType::USHORT: return "ushort";
+	case parMemberType::INT: return "int";
+	case parMemberType::UINT: return "uint";
+	case parMemberType::FLOAT: return "float";
+	case parMemberType::VEC2V: return "vec2V";
+	case parMemberType::VECTOR2: return "vec2";
+	case parMemberType::VEC3V:  return "vec3V";
+	case parMemberType::VECTOR3: return "vec3";
+	case parMemberType::VEC4V:  return "vec4V";
+	case parMemberType::VECTOR4: return "vec4";
+	case parMemberType::STRING: return "string";
+	case parMemberType::STRUCT: return "struct";
+	case parMemberType::ARRAY: return "array";
+	case parMemberType::ENUM: return "enum";
+	case parMemberType::BITSET: return "bitset";
+	case parMemberType::MAP: return "map";
+	case parMemberType::MAT34V: return "matrix34V";
+	case parMemberType::MATRIX34: return "matrix34";
+	case parMemberType::MAT44V: return "matrix44V";
+	case parMemberType::MATRIX44: return "matrix44";
+	case parMemberType::MAT33V: return "matrix33V";
+	case parMemberType::SCALARV: return "scalarV";
+	case parMemberType::BOOLV: return "boolV";
+	case parMemberType::VECBOOLV: return "vecBoolV";
+	case parMemberType::PTRDIFFT: return "ptrdiff_t";
+	case parMemberType::SIZET: return "size_t";
+	case parMemberType::FLOAT16: return "float16";
+	case parMemberType::INT64: return "int64";
+	case parMemberType::UINT64: return "uint64";
+	case parMemberType::DOUBLE: return "double";
+#if RDR3
+	case parMemberType::GUID: return "guid";
+	case parMemberType::_0xFE5A582C: return "_0xFE5A582C";
+	case parMemberType::QUATV: return "quatV";
+#endif
+	default: return "UNKNOWN";
+	}
+}
+
+static void FlagsToStringAppend(std::string& s, const char* flagStr)
+{
+	if (s.size() > 0) s += ", ";
+	s += flagStr;
+}
+
+#define FLAG_APPEND(flag) if ((flags & FLAG_TYPE::flag) == FLAG_TYPE::flag) FlagsToStringAppend(result, #flag)
+
+std::string FlagsToString(parEnumFlags flags)
+{
+	std::string result = "";
+#define FLAG_TYPE parEnumFlags
+	FLAG_APPEND(ENUM_STATIC);
+	FLAG_APPEND(ENUM_HAS_NAMES);
+	FLAG_APPEND(ENUM_ALWAYS_HAS_NAMES);
+#undef FLAG_TYPE
+	return result;
+}
+
+std::string FlagsToString(parStructure::Flags flags)
+{
+	std::string result = "";
+#define FLAG_TYPE parStructure::Flags
+	FLAG_APPEND(_0xB9C5D274);
+	FLAG_APPEND(HAS_NAMES);
+	FLAG_APPEND(ALWAYS_HAS_NAMES);
+	FLAG_APPEND(_0x25CB183C);
+	FLAG_APPEND(_0x62BE3669);
+	FLAG_APPEND(_0x22A1FBDB);
+#undef FLAG_TYPE
+	return result;
+}
+
+std::string FlagsToString(parMemberArrayData::AllocFlags flags)
+{
+	std::string result = "";
+#define FLAG_TYPE parMemberArrayData::AllocFlags
+	FLAG_APPEND(USE_PHYSICAL_ALLOCATOR);
+#undef FLAG_TYPE
+	return result;
+}
+
+#undef FLAG_APPEND
---
+++ src/rage.h
@@ -0,0 +1,385 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <Windows.h>
+
+
+template<class T>
+struct atArray
+{
+	T* Items;
+	uint16_t Count;
+	uint16_t Size;
+};
+
+template<class TKey, class TValue>
+struct atMap
+{
+	struct Entry
+	{
+		TKey key;
+		TValue value;
+		Entry* next;
+	};
+
+	Entry** Buckets;
+	uint16_t NumBuckets;
+	uint16_t NumEntries;
+	int8_t field_C[3];
+	bool IsResizable;
+};
+
+template<class TKey, class TValue>
+struct atBinaryMap
+{
+	struct DataPair
+	{
+		TKey Key;
+		TValue Value;
+	};
+
+	bool IsSorted;
+	atArray<DataPair> Pairs;
+};
+
+
+struct parDelegateHolderBase
+{
+	void* arg;
+	void* func;
+};
+
+enum class parMemberType : uint8_t // 0x1CA39C3D
+{
+	BOOL = 0,
+	CHAR = 1,
+	UCHAR = 2,
+	SHORT = 3,
+	USHORT = 4,
+	INT = 5,
+	UINT = 6,
+	FLOAT = 7,
+	VECTOR2 = 8,
+	VECTOR3 = 9,
+	VECTOR4 = 10,
+	STRING = 11,
+	STRUCT = 12,
+	ARRAY = 13,
+	ENUM = 14,
+	BITSET = 15,
+	MAP = 16,
+	MATRIX34 = 17,
+	MATRIX44 = 18,
+	VEC2V = 19,
+	VEC3V = 20,
+	VEC4V = 21,
+	MAT33V = 22,
+	MAT34V = 23,
+	MAT44V = 24,
+	SCALARV = 25,
+	BOOLV = 26,
+	VECBOOLV = 27,
+	PTRDIFFT = 28,
+	SIZET = 29,
+	FLOAT16 = 30,
+	INT64 = 31,
+	UINT64 = 32,
+	DOUBLE = 33,
+#if RDR3
+	GUID = 34,
+	_0xFE5A582C = 35,
+	QUATV = 36,
+#endif
+};
+
+enum class parMemberCommonSubtype
+{
+	// these don't seem to be used while parsing, probably used by internal engine tools
+	COLOR = 1, // used with UINT, VECTOR3 (RDR3)
+#if RDR3
+	ANGLE = 2, // used with FLOAT
+#endif
+};
+
+enum class parMemberArraySubtype  // 0xADE25B1B
+{
+	ATARRAY = 0,                        // 0xABE40192
+	ATFIXEDARRAY = 1,                   // 0x3A523E81
+	ATRANGEARRAY = 2,                   // 0x18A25B6B
+	POINTER = 3,                        // 0x47073D6E
+	MEMBER = 4,                         // 0x6CC11BB4
+	_0x2087BB00 = 5,                    // 0x2087BB00 - 32-bit atArray
+	POINTER_WITH_COUNT = 6,             // 0xE2980EB5
+	POINTER_WITH_COUNT_8BIT_IDX = 7,    // 0x254D33B1
+	POINTER_WITH_COUNT_16BIT_IDX = 8,   // 0xB66B6752
+	VIRTUAL = 9,                        // 0xAC01A1DC
+};
+
+enum class parMemberEnumSubtype  // 0x2721C60A
+{
+#if RDR3
+	_64BIT = 0,
+	_32BIT = 1,
+	_16BIT = 2,
+	_8BIT = 3,
+#else
+	_32BIT = 0,         // 0xAF085554
+	_16BIT = 1,         // 0x0D502D8E
+	_8BIT = 2,          // 0xF2AAF53D
+#endif
+};
+
+enum class parMemberBitsetSubtype
+{
+#if RDR3
+	_32BIT = 0,         // 0x4A4F3BEC
+	_16BIT = 1,         // 0x16434158
+	_8BIT = 2,          // 0x2EFEF517
+	ATBITSET = 3,       // 0xB46B5F65
+	_64BIT = 4,         // 0x3BB5B764
+#else
+	_32BIT = 0,         // 0xAF085554
+	_16BIT = 1,         // 0x0D502D8E
+	_8BIT = 2,          // 0xF2AAF53D
+	ATBITSET = 3,       // 0xB46B5F65
+#endif
+};
+
+enum class parMemberMapSubtype  // 0x9C9F1983
+{
+	ATMAP = 0,          // 0xD8C10171
+	ATBINARYMAP = 1,    // 0x6560BA79
+};
+
+enum class parMemberStringSubtype  // 0xA5CF41A9
+{
+	MEMBER = 0,                 // 0x6CC11BB4
+	POINTER = 1,                // 0x47073D6E
+	CONST_STRING = 2,           // 0x757C1B9B
+	ATSTRING = 3,               // 0x5CDCA61E
+	WIDE_MEMBER = 4,            // 0xAC508104
+	WIDE_POINTER = 5,           // 0x99D4A8CD
+	ATWIDESTRING = 6,           // 0x3DED5509
+	ATNONFINALHASHSTRING = 7,   // 0xDFE6E4AF
+	ATFINALHASHSTRING = 8,      // 0x945E5945
+	ATHASHVALUE = 9,            // 0xBD3CD157
+	ATPARTIALHASHVALUE = 10,    // 0xD552B3C8
+	ATNSHASHSTRING = 11,        // 0x893F9F69
+	ATNSHASHVALUE = 12,         // 0x3767C917
+#if RDR3
+	ATHASHVALUE16U = 13,           // 0xE8282E2F
+#endif
+};
+
+enum class parMemberStructSubtype  // 0x76214E40
+{
+	STRUCTURE = 0,                  // 0x3AC3050F
+	EXTERNAL_NAMED = 1,             // 0xA53F8BA9
+	EXTERNAL_NAMED_USERNULL = 2,    // 0x2DED4C19
+	POINTER = 3,                    // 0x47073D6E
+	SIMPLE_POINTER = 4,             // 0x67466543
+};
+
+#if RDR3
+enum class parMemberGuidSubtype  // 0xA73F91EB
+{
+	_0xDF7EBE85 = 0, // 0xDF7EBE85
+};
+#endif
+
+struct parMemberCommonData
+{
+	uint32_t name;
+	uint8_t padding4[4];
+	uint64_t offset;
+	parMemberType type;
+	uint8_t subType;
+	uint16_t flags1;
+	uint16_t flags2;
+	uint16_t extraData; // specific to parMemberCommonData derived types
+	void* attributes;
+};
+
+struct parMemberSimpleData : public parMemberCommonData
+{
+#if RDR3
+	double initValue;
+#else
+	float initValue;
+#endif
+};
+
+struct parMemberStringData : public parMemberCommonData
+{
+	uint32_t memberSize; // for subtype MEMBER and WIDE_MEMBER
+
+	uint8_t GetNamespaceIndex() { return static_cast<uint8_t>(extraData); }
+};
+
+struct parMemberStructData : public parMemberCommonData
+{
+	using ExternalNamedResolveCallback = void*(*)(const char* name);
+	using ExternalNamedGetNameCallback = const char*(*)(void* structPtr);
+	using AllocateStructCallback = void*(*)(void* treeNode);
+
+	struct parStructure* structure;
+	ExternalNamedResolveCallback externalNamedResolve;
+	ExternalNamedGetNameCallback externalNamedGetName;
+	AllocateStructCallback allocateStruct;
+};
+
+struct parMemberEnumData : public parMemberCommonData
+{
+#if RDR3
+	uint64_t initValue;
+#else
+	uint32_t initValue;
+	uint32_t padding24;
+#endif
+	struct parEnumData* enumData;
+	uint16_t valueCount;
+	uint8_t padding32[0x6];
+};
+
+struct parMemberArrayData : public parMemberCommonData
+{
+	enum class AllocFlags : uint16_t
+	{
+		USE_PHYSICAL_ALLOCATOR = 1 << 0,
+	};
+
+	uint64_t itemByteSize;
+	union
+	{
+		uint32_t arraySize;
+		uint32_t countOffset; // for POINTER_WITH_COUNT types
+	};
+	uint32_t padding2C;
+	parMemberCommonData* itemData;
+	parDelegateHolderBase* virtualCallback;
+
+	AllocFlags GetAllocFlags() { return static_cast<AllocFlags>(extraData); }
+};
+DEFINE_ENUM_FLAG_OPERATORS(parMemberArrayData::AllocFlags);
+
+struct parMemberMapData : public parMemberCommonData
+{
+	parDelegateHolderBase* createIterator;
+	parDelegateHolderBase* createInterface;
+	parMemberCommonData* keyData;
+	parMemberCommonData* valueData;
+};
+
+struct parMember
+{
+	void* __vftable;
+	parMemberCommonData* data;
+};
+
+struct parStructure
+{
+	enum class Flags
+#if RDR3
+		: uint32_t
+#else
+		: uint16_t
+#endif
+	{
+		_0xB9C5D274 = 1 << 0, // 0xB9C5D274
+		HAS_NAMES = 1 << 1, // 0x47AF4932
+		ALWAYS_HAS_NAMES = 1 << 2, // 0x9804A870
+		_0x25CB183C = 1 << 3, // 0x25CB183C
+		_0x62BE3669 = 1 << 4, // 0x62BE3669
+		_0x22A1FBDB = 1 << 5, // 0x22A1FBDB
+	};
+
+	void* __vftable;
+#if RDR3
+	uint8_t critSection[0x28];
+#endif
+	uint32_t name;
+	uint8_t padding[4];
+	parStructure* baseStructure;
+	uint64_t baseOffset;
+	uint64_t structureSize;
+	Flags flags;
+	uint16_t alignment;
+	uint16_t versionMajor;
+	uint16_t versionMinor;
+#if RDR3
+	uint8_t padding5A[6];
+#endif
+	atArray<parMember*> members;
+	void* extraAttributes;
+	parDelegateHolderBase factoryNew;
+	parDelegateHolderBase factoryPlacementNew;
+	parDelegateHolderBase getStructureCB;
+	parDelegateHolderBase factoryDelete;
+	atBinaryMap<uint32_t, parDelegateHolderBase*> callbacks;
+};
+DEFINE_ENUM_FLAG_OPERATORS(parStructure::Flags);
+
+struct parStructureStaticData
+{
+	uint32_t name;
+	uint8_t padding[4];
+	const char* nameStr;
+	parStructure* parser;
+	parMemberCommonData** membersData;
+	uint32_t* membersOffsets;
+	const char** memberNames;
+	bool unkFlag1;
+	bool unkFlag2;
+};
+
+
+struct parEnumValueData
+{
+#if RDR3
+	uint32_t name;
+	uint32_t padding4;
+	uint64_t value;
+#else
+	uint32_t name;
+	uint32_t value;
+#endif
+};
+
+enum class parEnumFlags : uint16_t
+{
+	ENUM_STATIC = 1 << 0, // 0x83E077B4
+	ENUM_HAS_NAMES = 1 << 1, // 0x4725AEB1
+	ENUM_ALWAYS_HAS_NAMES = 1 << 2, // 0x0227ED1D
+};
+DEFINE_ENUM_FLAG_OPERATORS(parEnumFlags);
+
+struct parEnumData
+{
+	parEnumValueData* values;
+	const char** valueNames;
+	uint16_t valueCount;
+	parEnumFlags flags;
+	uint32_t name;
+};
+
+struct parManager
+{
+#if RDR3
+	uint8_t padding[0x48];
+#else
+	uint8_t padding[0x30];
+#endif
+	atMap<uint32_t, parStructure*> structures;
+	// ...
+
+	static parManager** sm_Instance;
+};
+
+
+std::string SubtypeToStr(parMemberType type, uint8_t subtype);
+const char* TypeToStr(parMemberType type);
+const char* TypeToCasedStr(parMemberType type);
+
+std::string FlagsToString(parEnumFlags flags);
+std::string FlagsToString(parStructure::Flags flags);
+std::string FlagsToString(parMemberArrayData::AllocFlags flags);
