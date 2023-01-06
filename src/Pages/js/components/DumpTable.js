f299638d2d9c11c491f195965ab64d0bfe659f6d
alexguirre <alexguirre.aa@gmail.com>
2023-01-06 03:56:52 +0100

    feat(pages): build selection to open diff viewer

--- src/Pages/js/components/DumpTable.js
+++ src/Pages/js/components/DumpTable.js
@@ -1,4 +1,4 @@
-import { gameIdToFormattedName } from '../util.js';
+import {gameIdToFormattedName, hideElement} from '../util.js';
 
 export default class DumpTable extends HTMLElement {
 
@@ -12,6 +12,7 @@ export default class DumpTable extends HTMLElement {
                         <th></th>
                         <th>Build</th>
                         <th>Aliases</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -28,12 +29,19 @@ export default class DumpTable extends HTMLElement {
             </td>
             <td>???</td>
             <td>–</td>
+            <td>
+                <button title="Select for compare">Select for compare</button>
+                <button title="Compare with selected" class="hidden">Compare with selected</button>
+            </td>
         </tr>
     `;
 
     #game;
     #body;
     #rowTemplate;
+    #onSelectForCompareHandler;
+    #onCompareWithSelectedHandler;
+    #buildSelectedForCompare;
 
     constructor(game) {
         super();
@@ -49,10 +57,15 @@ export default class DumpTable extends HTMLElement {
         const header = shadow.querySelector("h2");
         header.innerHTML = gameIdToFormattedName(game);
         this.#body = shadow.querySelector("tbody");
+
+        this.#onSelectForCompareHandler = this.#onSelectForCompare.bind(this);
+        this.#onCompareWithSelectedHandler = this.#onCompareWithSelected.bind(this);
+        this.#buildSelectedForCompare = null;
     }
 
     addRow(build, aliases) {
         this.#body.appendChild(this.#createRow(build, aliases));
+        this.#body.lastElementChild.dataset.build = build;
     }
 
     #createRow(build, aliases) {
@@ -68,7 +81,48 @@ export default class DumpTable extends HTMLElement {
         downloads.setAttribute("game", this.#game);
         downloads.setAttribute("build", build);
 
+        cols[3].querySelector("button:first-child").addEventListener("click", this.#onSelectForCompareHandler);
+        cols[3].querySelector("button:last-child").addEventListener("click", this.#onCompareWithSelectedHandler);
+
         return row;
+    }
+
+    #onSelectForCompare(e) {
+        console.log(e);
+        console.log(e.target);
+        const isSelected = e.target.dataset.selected !== undefined;
+        if (isSelected) {
+            // unselect
+            this.#buildSelectedForCompare = null;
+            delete e.target.dataset.selected;
+            this.#body.querySelectorAll("tr > td:nth-child(4) > button:first-child").forEach(b => hideElement(b, false));
+            this.#body.querySelectorAll("tr > td:nth-child(4) > button:last-child").forEach(b => hideElement(b, true));
+        } else {
+            // select
+            this.#buildSelectedForCompare = e.target.parentElement.parentElement.dataset.build;
+            e.target.dataset.selected = "";
+            this.#body.querySelectorAll("tr > td:nth-child(4) > button:first-child").forEach(b => {
+                if (b !== e.target) {
+                    hideElement(b, true);
+                }
+            });
+            this.#body.querySelectorAll("tr > td:nth-child(4) > button:last-child").forEach(b => {
+                if (b.parentElement !== e.target.parentElement) {
+                    hideElement(b, false);
+                }
+            });
+        }
+    }
+
+    #onCompareWithSelected(e) {
+        if (this.#buildSelectedForCompare === null) {
+            return;
+        }
+
+        const buildA = this.#buildSelectedForCompare;
+        const buildB = e.target.parentElement.parentElement.dataset.build;
+
+        window.location = `diff.html?game=${this.#game}&build-a=${buildA}&build-b=${buildB}`;
     }
 }
 customElements.define('dump-table', DumpTable);
